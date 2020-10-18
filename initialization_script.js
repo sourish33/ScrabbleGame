2bbfd8a9f1e4936f3f5ec12f04cc6c204a47745b
Sourish Dutta <sourishdutta@Sourishs-MBP.attlocal.net>
2020-10-17 20:45:25 -0500

    addHuman button works so far (nothing else does)

--- initialization_script.js
+++ initialization_script.js
@@ -12,28 +12,44 @@ function hide(x){
     x.classList.add("not-there")
 }
 
+let players=[]
+
 
 function addPlayer(){
-    f3 = document.getElementById("3rdplayer");
-    f4 = document.getElementById("4thplayer");
-    button1 = document.getElementById("addplayerbutton");
-    button2 = document.getElementById("removeplayerbutton");
-    f3invisible=f3.classList.contains("not-there");
-    f4invisible = f4.classList.contains("not-there");
-    f3visible = !f3invisible;
-    f4visible = !f4invisible;
+    let f1= document.getElementById("1stplayer");
+    let f2 = document.getElementById("2ndplayer");
+    let f3 = document.getElementById("3rdplayer");
+    let f4 = document.getElementById("4thplayer");
+    let fs=[f1,f2,f3,f4]
+    let button1 = document.getElementById("addH");
+    let button2 = document.getElementById("addC");
+    let button3 = document.getElementById("removeplayerbutton");
+    // let f1invisible=f1.classList.contains("not-there");
+    // let f2invisible = f2.classList.contains("not-there");
+    // let f3invisible=f3.classList.contains("not-there");
+    // let f4invisible = f4.classList.contains("not-there");
+    // let f1visible = !f1invisible;
+    // let f2visible = !f2invisible;
+    // let f3visible = !f3invisible;
+    // let f4visible = !f4invisible;
 
-    if (f3invisible && f4invisible){
-        show(f3)
-        show(button2)
-        
+    if (players.length===0){
+        show(fs[0])
+        players[0]=fs[0]
+        show(button3)
+        return 
     }
 
-    if (f3visible && f4invisible){
-        show(f4)
+    if (Object.keys(players).length===4){
         hide(button1)
+        hide(button2)
+        return
     }
-
+    else {
+        let v=players.length;
+        show(fs[v])
+        players.push(fs[v])
+    }
 
 }
 
@@ -41,7 +57,7 @@ function removePlayer(){////WORKING ON THIS!!!
     /////TODO: Reset a player when removing them
     f3 = document.getElementById("3rdplayer");
     f4 = document.getElementById("4thplayer");
-    button1 = document.getElementById("addplayerbutton");
+    button1 = document.getElementById("addH");
     button2 = document.getElementById("removeplayerbutton");
     f3invisible=f3.classList.contains("not-there");
     f4invisible = f4.classList.contains("not-there");
@@ -78,7 +94,6 @@ function getRadioValue(name){
 
 function showAILevel(n){
 
-    let check=document.querySelector(`input[name=ck${n}]`)
     let level = document.getElementById(`AIlevel${n}`)
     if (check.checked){
         level.classList.remove("ghost")
@@ -90,7 +105,6 @@ function showAILevel(n){
 function resetPlayer(i){
     document.getElementById(`player${i}`).value ="";
     document.getElementById(`p${i}`).value="1";
-    document.querySelector(`input[name=ck${i}]`).checked=false;
 }
 
 function resetPage(){
@@ -131,15 +145,15 @@ function getInitialData() {
 
     }
     //creating AI List
-    let AIList = []
-    for (let i=1;i<5;i++){
+    // let AIList = []
+    // for (let i=1;i<5;i++){
        
-        if (document.querySelector(`input[name=ck${i}]`).checked==true){
-            AIList.push(document.getElementById(`p${i}`).value)
-        } else{
-            AIList.push("0")
-        }
-    }
+    //     if (document.querySelector(`input[name=ck${i}]`).checked==true){
+    //         AIList.push(document.getElementById(`p${i}`).value)
+    //     } else{
+    //         AIList.push("0")
+    //     }
+    // }
 
     
     sessionStorage.setItem('AIList', JSON.stringify(AIList))
@@ -159,12 +173,7 @@ function getInitialData() {
 // });
 
 document.getElementById("playbutton").addEventListener("click", getInitialData);
-document.getElementById("addplayerbutton").addEventListener("click", addPlayer);
+document.getElementById("addH").addEventListener("click", addPlayer);
 document.getElementById("removeplayerbutton").addEventListener('click', removePlayer);
-for (let i=1;i<5;i++){
-    document.querySelector(`input[name=ck${i}]`).addEventListener("change", ()=>{
-        showAILevel(i);
-    });
-}
 resetPage()
 
